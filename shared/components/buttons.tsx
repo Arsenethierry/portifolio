50deb85f5782043d85918708d1b15c06f17302bb
Arsene <komezarsene15@gmail.com>
2023-03-02 02:13:02 +0530

    home page

--- shared/components/buttons.tsx
+++ shared/components/buttons.tsx
@@ -1,15 +1,17 @@
 interface Props {
     text: string;
     type: 'solid' | 'outlined';
-    onClickHandler: () => void;
+    // onClickHandler: () => void;
   }
 
-export default function Button({ text, type, onClickHandler = () => {} }: Props): JSX.Element {
+export default function Button({ text, type,
+    //  onClickHandler = () => {} 
+    }: Props): JSX.Element {
     return(
         <>
         <button
             type="button"
-            onClick={onClickHandler}
+            // onClick={onClickHandler}
             className={
                 `font-regular
                 px-6 sm:px-4 xl:px-6 py-4 w-64 sm:w-52 md:w-56 2xl:w-60 text-7xl h-25 transition-all
